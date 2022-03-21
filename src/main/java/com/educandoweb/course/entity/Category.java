906c885638660455b52e7c3379b877b556c2f2e5
Arthur Cech Francisco <arthurcech@gmail.com>
2022-03-21 15:21:59 -0300

    feat: ProductEntity; ProductController; ProductService; ProductRepository

--- src/main/java/com/educandoweb/course/entity/Category.java
+++ src/main/java/com/educandoweb/course/entity/Category.java
@@ -1,12 +1,10 @@
 package com.educandoweb.course.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -19,6 +17,9 @@ public class Category implements Serializable {
     private Long id;
 
     private String name;
+
+    @Transient
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -42,6 +43,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
