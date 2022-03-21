9f6c5070479f9805a743615454ad88bb041753a0
Arthur Cech Francisco <arthurcech@gmail.com>
2022-03-21 14:00:24 -0300

    feat: UserRepository; database seeding com CommandLinerRunner no perfil de testes

---
+++ src/main/java/com/educandoweb/course/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.educandoweb.course.repository;
+
+import com.educandoweb.course.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
