893a4b8fcd3afc9f0e07f2ec322f4bc44ce1843d
Arthur Cech Francisco <arthurcech@gmail.com>
2022-03-22 10:13:06 -0300

    feat: implementado excecao personalizada e seu tratamento para findById

---
+++ src/main/java/com/educandoweb/course/controller/exception/ResourceExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.educandoweb.course.controller.exception;
+
+import com.educandoweb.course.service.exception.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(
+            ResourceNotFoundException exception, HttpServletRequest request) {
+
+        String error = "resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError standardError = new StandardError(Instant.now(),
+                status.value(),
+                error,
+                exception.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(status).body(standardError);
+    }
+
+}
--- src/main/java/com/educandoweb/course/service/UserService.java
+++ src/main/java/com/educandoweb/course/service/UserService.java
@@ -2,6 +2,7 @@ package com.educandoweb.course.service;
 
 import com.educandoweb.course.entity.User;
 import com.educandoweb.course.repository.UserRepository;
+import com.educandoweb.course.service.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
-        return user.get();
+        return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User user) {
