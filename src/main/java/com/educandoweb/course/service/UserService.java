c4c62b99f727ea1af8d9c9974d34793019198acc
Arthur Cech Francisco <arthurcech@gmail.com>
2022-03-22 10:01:32 -0300

    refactor: implementado operacao de atualizar User

--- src/main/java/com/educandoweb/course/service/UserService.java
+++ src/main/java/com/educandoweb/course/service/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
         userRepository.deleteById(id);
     }
 
+    public User update(Long id, User user) {
+        User entity = userRepository.getOne(id); // prepara o objeto monitorado
+        updateData(entity, user);
+        return userRepository.save(entity);
+    }
+
+    private void updateData(User entity, User user) {
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPhone(user.getPhone());
+    }
+
 }
