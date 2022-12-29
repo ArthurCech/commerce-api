43b3f4c49b8d5ec7ccb953d28992e07bb8824839
Arthur Cech Francisco <arthurcech@gmail.com>
2022-12-29 15:54:34 -0300

    feat: create, update and delete Product

--- src/main/java/io/github/arthurcech/orderscrudcommerce/controller/ProductController.java
+++ src/main/java/io/github/arthurcech/orderscrudcommerce/controller/ProductController.java
@@ -1,15 +1,22 @@
 package io.github.arthurcech.orderscrudcommerce.controller;
 
-import java.util.List;
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import io.github.arthurcech.orderscrudcommerce.entity.Product;
+import io.github.arthurcech.orderscrudcommerce.dto.ProductDTO;
 import io.github.arthurcech.orderscrudcommerce.service.ProductService;
 
 @RestController
@@ -20,13 +27,32 @@ public class ProductController {
 	private ProductService service;
 
 	@GetMapping
-	public ResponseEntity<List<Product>> findAll() {
-		return ResponseEntity.ok().body(service.findAll());
+	public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+		return ResponseEntity.ok().body(service.findAll(pageable));
 	}
 
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Product> findById(@PathVariable Long id) {
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		return ResponseEntity.ok().body(service.findById(id));
+	}
+
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok(dto);
 	}
 
 }
--- src/main/java/io/github/arthurcech/orderscrudcommerce/service/ProductService.java
+++ src/main/java/io/github/arthurcech/orderscrudcommerce/service/ProductService.java
@@ -1,13 +1,21 @@
 package io.github.arthurcech.orderscrudcommerce.service;
 
-import java.util.List;
-import java.util.Optional;
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import io.github.arthurcech.orderscrudcommerce.dto.CategoryDTO;
+import io.github.arthurcech.orderscrudcommerce.dto.ProductDTO;
+import io.github.arthurcech.orderscrudcommerce.entity.Category;
 import io.github.arthurcech.orderscrudcommerce.entity.Product;
+import io.github.arthurcech.orderscrudcommerce.repository.CategoryRepository;
 import io.github.arthurcech.orderscrudcommerce.repository.ProductRepository;
+import io.github.arthurcech.orderscrudcommerce.service.exception.DatabaseException;
 import io.github.arthurcech.orderscrudcommerce.service.exception.ResourceNotFoundException;
 
 @Service
@@ -15,14 +23,58 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 
-	public List<Product> findAll() {
-		return repository.findAll();
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> products = repository.findAll(pageable);
+		return products.map(product -> new ProductDTO(product));
 	}
 
-	public Product findById(Long id) {
-		Optional<Product> product = repository.findById(id);
-		return product.orElseThrow(() -> new ResourceNotFoundException(id));
+	public ProductDTO findById(Long id) {
+		Product product = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
+		return new ProductDTO(product, product.getCategories());
+	}
+
+	public ProductDTO insert(ProductDTO dto) {
+		Product product = new Product();
+		dtoToProduct(dto, product);
+		product = repository.save(product);
+		return new ProductDTO(product, product.getCategories());
+	}
+
+	public ProductDTO update(Long id, ProductDTO dto) {
+		try {
+			Product product = repository.getById(id);
+			dtoToProduct(dto, product);
+			product = repository.save(product);
+			return new ProductDTO(product, product.getCategories());
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	private void dtoToProduct(ProductDTO dto, Product product) {
+		product.setName(dto.getName());
+		product.setDescription(dto.getDescription());
+		product.setPrice(dto.getPrice());
+		product.setImgUrl(dto.getImgUrl());
+
+		product.getCategories().clear();
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getById(catDto.getId());
+			product.getCategories().add(category);
+		}
 	}
 
 }
