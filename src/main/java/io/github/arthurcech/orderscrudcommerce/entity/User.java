72cbb157eba05b306dd368aa2d2bd3d6e104cc2d
Arthur Cech Francisco <arthurcech@gmail.com>
2023-02-14 08:31:36 -0300

    feat(user): add Role and implement UserDetails

--- src/main/java/io/github/arthurcech/orderscrudcommerce/entity/User.java
+++ src/main/java/io/github/arthurcech/orderscrudcommerce/entity/User.java
@@ -1,30 +1,44 @@
 package io.github.arthurcech.orderscrudcommerce.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.github.arthurcech.orderscrudcommerce.entity.enums.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User implements Serializable, UserDetails {
 
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
     private Long id;
     @Column(nullable = false)
     private String name;
@@ -34,6 +48,10 @@ public class User implements Serializable {
     private String phone;
     @Column(nullable = false)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     @CreationTimestamp
     private Instant createdAt;
@@ -41,28 +59,8 @@ public class User implements Serializable {
     @UpdateTimestamp
     private Instant updatedAt;
 
-    @JsonIgnore
     @OneToMany(mappedBy = "client")
     private Set<Order> orders = new HashSet<>();
-
-    public User() {
-    }
-
-    public User(Long id,
-                String name,
-                String email,
-                String phone,
-                String password,
-                Instant createdAt,
-                Instant updatedAt) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.phone = phone;
-        this.password = password;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
 
     public Long getId() {
         return id;
@@ -96,12 +94,21 @@ public class User implements Serializable {
         this.phone = phone;
     }
 
+    @Override
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     public Instant getCreatedAt() {
@@ -114,6 +121,36 @@ public class User implements Serializable {
 
     public Set<Order> getOrders() {
         return orders;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     @Override
---
+++ src/main/java/io/github/arthurcech/orderscrudcommerce/entity/enums/Role.java
@@ -0,0 +1,8 @@
+package io.github.arthurcech.orderscrudcommerce.entity.enums;
+
+public enum Role {
+
+    USER,
+    ADMIN
+
+}
