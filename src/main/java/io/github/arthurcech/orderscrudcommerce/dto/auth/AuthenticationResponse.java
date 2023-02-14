b33b5ab8f9904a42be1cf207804bbb65352c528a
Arthur Cech Francisco <arthurcech@gmail.com>
2023-02-14 14:05:29 -0300

    refactor: return user role when register and log in

--- src/main/java/io/github/arthurcech/orderscrudcommerce/dto/auth/AuthenticationResponse.java
+++ src/main/java/io/github/arthurcech/orderscrudcommerce/dto/auth/AuthenticationResponse.java
@@ -1,11 +1,13 @@
 package io.github.arthurcech.orderscrudcommerce.dto.auth;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import io.github.arthurcech.orderscrudcommerce.entity.enums.Role;
 
-@JsonPropertyOrder({"name", "email", "phone"})
+@JsonPropertyOrder({"name", "email", "phone", "role"})
 public record AuthenticationResponse(
         String name,
         String email,
-        String phone
+        String phone,
+        Role role
 ) {
 }
